72c2687cd33ae7bcdf07d1c5d8933ff0eb0246ea
Mila <ludmigola@gmail.com>
2023-05-07 11:49:17 +0200

    Исходные файлы для телеграмм бота

---
+++ config.py
@@ -0,0 +1,6 @@
+TOKEN = '6009272693:AAHVFWzFjcfDhgswdxG8Q9BdNF-cbbN0c-s'
+keys = {
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+}
---
+++ extensions.py
@@ -0,0 +1,39 @@
+import requests
+import json
+from config import keys
+
+
+class APIException(Exception):
+    pass
+
+
+class CurrencyConverter:
+    @staticmethod
+    def get_price(quote: str, base: str, amount: str):
+        if quote == base:
+            raise APIException(f'Невозможно перевести одинаковые валюты {base}')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {quote}')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {base}')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f'Не удалось обработать количество {amount}')
+
+        if amount <= 0:
+            raise APIException(f'Количество запрашиваемой валюты {amount} должно быть больше 0')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+
+        total_base = json.loads(r.content)[keys[base]]
+        total_base *= amount
+
+        return total_base
